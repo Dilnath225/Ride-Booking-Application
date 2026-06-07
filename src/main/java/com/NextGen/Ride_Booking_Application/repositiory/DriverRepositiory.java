6d92678816192254734a331aa28450035670dbcf
Dilnath225 <sunerarathnayaka@gmail.com>
2026-06-07 19:40:26 +0530

    feat: add DriverRepository with findByAvailable method

--- src/main/java/com/NextGen/Ride_Booking_Application/repositiory/DriverRepositiory.java
+++ src/main/java/com/NextGen/Ride_Booking_Application/repositiory/DriverRepositiory.java
@@ -1,4 +1,19 @@
 package com.NextGen.Ride_Booking_Application.repositiory;
+import com.NextGen.Ride_Booking_Application.entity.Driver;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 public class DriverRepositiory {
+
+    @Repository
+    public interface DriverRepository extends JpaRepository <Driver, Long>{
+
+        List<Driver> findByAvailable(boolean available);
+
+
+    }
+
+
 }
