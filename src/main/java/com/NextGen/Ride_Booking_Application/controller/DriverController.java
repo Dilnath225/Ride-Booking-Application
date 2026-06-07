a7b01b5853b6c600de66f002adcb96a332b34334
Dilnath225 <sunerarathnayaka@gmail.com>
2026-06-07 21:35:47 +0530

    created driver controller file

--- src/main/java/com/NextGen/Ride_Booking_Application/controller/DriverController.java
+++ src/main/java/com/NextGen/Ride_Booking_Application/controller/DriverController.java
@@ -1,4 +1,36 @@
 package com.NextGen.Ride_Booking_Application.controller;
+import com.NextGen.Ride_Booking_Application.entity.Driver;
+import com.NextGen.Ride_Booking_Application.service.DriverService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/drivers")
+@CrossOrigin(origins = "*") // to get the access for the frontend
 
 public class DriverController {
+    @Autowired
+    private DriverService driverService;
+
+    //to add a new driver in the system(POST method)
+    @PostMapping("/add")
+    public Driver addDriver(@RequestBody Driver driver){
+        return driverService.addDriver(driver);
+    }
+
+    //to check the available drivers(GET method)
+    @GetMapping("/available")
+    public List<Driver> getAvailableDrivers(){
+        return driverService.getAvailableDrivers();
+
+    }
+
+    //to check the driver status
+    @PutMapping("/{id}/status")
+    public Driver updateDriverStatus(@PathVariable Long id, @RequestParam boolean isAvailable) {
+        return driverService.updateDriverStatus(id, isAvailable);
+    }
+
+
 }
