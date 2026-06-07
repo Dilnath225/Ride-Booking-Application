0c26e3751b1f2a95416fa602dba52db1455123ad
Dilnath225 <sunerarathnayaka@gmail.com>
2026-06-07 21:04:58 +0530

    update the driver repositiory java file

---
+++ src/main/java/com/NextGen/Ride_Booking_Application/repository/DriverRepository.java
@@ -0,0 +1,12 @@
+package com.NextGen.Ride_Booking_Application.repository;
+
+import com.NextGen.Ride_Booking_Application.entity.Driver;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface DriverRepository extends JpaRepository<Driver, Long> {
+    List<Driver> findByAvailable(boolean available);
+}
