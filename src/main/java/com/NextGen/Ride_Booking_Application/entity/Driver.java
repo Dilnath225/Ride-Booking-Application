949266dd99c6d3d5fd5f12e2b207739c6c4723ba
Dilnath225 <sunerarathnayaka@gmail.com>
2026-06-07 19:31:56 +0530

    created drivers class: add drivers details.

--- src/main/java/com/NextGen/Ride_Booking_Application/entity/Driver.java
+++ src/main/java/com/NextGen/Ride_Booking_Application/entity/Driver.java
@@ -1,4 +1,22 @@
 package com.NextGen.Ride_Booking_Application.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name= "drivers")
+
 public class Driver {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String name;
+    private String vehicaleModel;
+    private String vehicleNumber;
+    private boolean available = true;
+
+
 }
