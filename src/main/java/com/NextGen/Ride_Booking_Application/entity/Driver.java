daa87b74019e3650c82552d3fd7b1e8cec0ab36e
Dilnath225 <sunerarathnayaka@gmail.com>
2026-06-07 21:06:40 +0530

    update the driver files to fix the error: add a manual set method

--- src/main/java/com/NextGen/Ride_Booking_Application/entity/Driver.java
+++ src/main/java/com/NextGen/Ride_Booking_Application/entity/Driver.java
@@ -19,4 +19,11 @@ public class Driver {
     private boolean available = true;
 
 
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
 }
