83dc7e773d96a124e51b7c51bcad5c6a15eb7499
Dilnath225 <sunerarathnayaka@gmail.com>
2026-06-08 12:40:29 +0530

    updated driver file

--- src/main/java/com/NextGen/Ride_Booking_Application/entity/Driver.java
+++ src/main/java/com/NextGen/Ride_Booking_Application/entity/Driver.java
@@ -1,29 +1,34 @@
 package com.NextGen.Ride_Booking_Application.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
-@Table(name= "drivers")
-
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "drivers")
 public class Driver {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
     private Long id;
+
     private String name;
-    private String vehicaleModel;
+
+    @Column(name = "vehicale_model")
+    private String vehicleModel;
+
     private String vehicleNumber;
-    private boolean available = true;
 
+    private Boolean available;
 
-    public boolean isAvailable() {
-        return available;
-    }
-
-    public void setAvailable(boolean available) {
-        this.available = available;
+    @PrePersist
+    public void prePersist() {
+        if (available == null) {
+            available = true;
+        }
     }
 }
