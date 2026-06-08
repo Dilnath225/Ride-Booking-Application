d39c2723b22a90de3830e4e4a7c83b8ce2693478
Dilnath225 <sunerarathnayaka@gmail.com>
2026-06-08 12:39:39 +0530

    updated driver controller file

--- src/main/java/com/NextGen/Ride_Booking_Application/controller/DriverController.java
+++ src/main/java/com/NextGen/Ride_Booking_Application/controller/DriverController.java
@@ -1,36 +1,45 @@
 package com.NextGen.Ride_Booking_Application.controller;
+
 import com.NextGen.Ride_Booking_Application.entity.Driver;
 import com.NextGen.Ride_Booking_Application.service.DriverService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/drivers")
 @CrossOrigin(origins = "*") // to get the access for the frontend
-
 public class DriverController {
+
     @Autowired
     private DriverService driverService;
 
-    //to add a new driver in the system(POST method)
+    // to add a new driver in the system (POST method)
     @PostMapping("/add")
-    public Driver addDriver(@RequestBody Driver driver){
-        return driverService.addDriver(driver);
+    public ResponseEntity<Driver> addDriver(@RequestBody Driver driver) {
+        Driver savedDriver = driverService.addDriver(driver);
+        return new ResponseEntity<>(savedDriver, HttpStatus.CREATED);
     }
 
-    //to check the available drivers(GET method)
+    // to get all drivers
+    @GetMapping
+    public ResponseEntity<List<Driver>> getAllDrivers() {
+        return ResponseEntity.ok(driverService.getAllDrivers());
+    }
+
+    // to check the available drivers (GET method)
     @GetMapping("/available")
-    public List<Driver> getAvailableDrivers(){
-        return driverService.getAvailableDrivers();
-
+    public ResponseEntity<List<Driver>> getAvailableDrivers() {
+        return ResponseEntity.ok(driverService.getAvailableDrivers());
     }
 
-    //to check the driver status
+    // to update the driver status
     @PutMapping("/{id}/status")
-    public Driver updateDriverStatus(@PathVariable Long id, @RequestParam boolean isAvailable) {
-        return driverService.updateDriverStatus(id, isAvailable);
+    public ResponseEntity<Driver> updateDriverStatus(@PathVariable Long id, @RequestParam boolean isAvailable) {
+        return ResponseEntity.ok(driverService.updateDriverStatus(id, isAvailable));
     }
-
-
 }
+
