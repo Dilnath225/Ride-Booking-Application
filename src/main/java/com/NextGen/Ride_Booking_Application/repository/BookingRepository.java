00da1f845d8ea63ba0aeeca6471a122d9f590978
Dilnath225 <sunerarathnayaka@gmail.com>
2026-06-08 12:58:57 +0530

    created booking repositor file

---
+++ src/main/java/com/NextGen/Ride_Booking_Application/repository/BookingRepository.java
@@ -0,0 +1,12 @@
+package com.NextGen.Ride_Booking_Application.repository;
+
+import com.NextGen.Ride_Booking_Application.entity.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+    List<Booking> findByPassengerName(String passengerName);
+}
