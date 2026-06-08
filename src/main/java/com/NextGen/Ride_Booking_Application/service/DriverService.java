b838daf8a2b282d540413949a9b930809d513dc7
Dilnath225 <sunerarathnayaka@gmail.com>
2026-06-08 12:32:43 +0530

    updated driver service file

--- src/main/java/com/NextGen/Ride_Booking_Application/service/DriverService.java
+++ src/main/java/com/NextGen/Ride_Booking_Application/service/DriverService.java
@@ -1,11 +1,12 @@
 package com.NextGen.Ride_Booking_Application.service;
+
 import com.NextGen.Ride_Booking_Application.entity.Driver;
+import com.NextGen.Ride_Booking_Application.exception.ResourceNotFoundException;
 import com.NextGen.Ride_Booking_Application.repository.DriverRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class DriverService {
@@ -13,26 +14,27 @@ public class DriverService {
     @Autowired
     private DriverRepository driverRepository;
 
-    //add a new driver to the system
-    public Driver addDriver(Driver driver){
-
+    // add a new driver to the system
+    public Driver addDriver(Driver driver) {
         return driverRepository.save(driver);
     }
 
-    //to  show the available drivers in the system
-    public List<Driver> getAvailableDrivers(){
+    // get all drivers in the system
+    public List<Driver> getAllDrivers() {
+        return driverRepository.findAll();
+    }
+
+    // show the available drivers in the system
+    public List<Driver> getAvailableDrivers() {
         return driverRepository.findByAvailable(true);
     }
 
-    //to update the driver status
-    public Driver updateDriverStatus(Long id, boolean isAvailable){
-        Optional<Driver> optionalDriver = driverRepository.findById(id);
-        if (optionalDriver.isPresent()){
-            Driver driver = optionalDriver.get();
-            driver.setAvailable(isAvailable);
-            return  driverRepository.save(driver);
-        }throw new RuntimeException("Driver is not found with relevant id" + id);
+    // update the driver status
+    public Driver updateDriverStatus(Long id, boolean isAvailable) {
+        Driver driver = driverRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Driver not found with id: " + id));
+        driver.setAvailable(isAvailable);
+        return driverRepository.save(driver);
     }
-
-
 }
+
